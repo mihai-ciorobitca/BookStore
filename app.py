70872503481d3592784461c7b330f39fedd158df
ciorobitcamihai <165281838+ciorobitcamihai@users.noreply.github.com>
2024-03-28 23:09:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,8 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from pymongo import MongoClient 
 from os import environ 
 
-mongo_uri = environ.get('MONGO_URI')  
+mongo_uri = "mongodb+srv://mihaiciorobitca:UtIekdcPUmWXB9rC@cluster.o1rs5cw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster"
+# environ.get('MONGO_URI')  
 print("KEY IS", mongo_uri)
 
 app = Flask(__name__)
